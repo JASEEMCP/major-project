7c2287f850558ba3ba9d1cc0ac206175ed43cc08
JASEEMCP <jaseemcp85@gmail.com>
2025-01-29 16:13:31 +0530

    feat: refactor ProfileSubmission to use StatefulWidget and enhance UI with custom text fields

--- web-student/lib/presentation/auth/sign_up/profile_submission.dart
+++ web-student/lib/presentation/auth/sign_up/profile_submission.dart
@@ -1,10 +1,72 @@
+import 'package:app/presentation/widget/custom_text_field.dart';
 import 'package:app/resource/utils/common_lib.dart';
 
-class ProfileSubmission extends StatelessWidget {
+class ProfileSubmission extends StatefulWidget {
   const ProfileSubmission({super.key});
 
   @override
+  State<ProfileSubmission> createState() => _ProfileSubmissionState();
+}
+
+class _ProfileSubmissionState extends State<ProfileSubmission> {
+  final List<TextEditingController> _textController = List.generate(
+    7,
+    (_) => generateTextController(),
+  );
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final inset = $style.insets;
+    return Scaffold(
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          spacing: inset.xs,
+          children: [
+            Gap(inset.offset),
+            Gap(inset.xs),
+            Align(
+              alignment: const Alignment(-0.1, 0),
+              child: Text(
+                'Complete\nUniversity Profile',
+                textAlign: TextAlign.left,
+                style: $style.text.textEB26.copyWith(),
+              ),
+            ),
+            Gap(inset.xs),
+            CustomTextField(
+              readOnly: true,
+              hint: 'University Name',
+              controller: _textController[0],
+            ),
+            CustomTextField(
+              readOnly: true,
+              hint: 'University email',
+              controller: _textController[1],
+            ),
+            CustomTextField(
+              readOnly: true,
+              hint: 'Short Name',
+              controller: _textController[2],
+            ),
+            CustomTextField(
+              readOnly: true,
+              hint: 'Phone Number',
+              controller: _textController[3],
+            ),
+            CustomTextField(
+              readOnly: true,
+              hint: 'Address',
+              controller: _textController[4],
+            ),
+            CustomTextField(
+              readOnly: true,
+              hint: 'Website',
+              controller: _textController[5],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
