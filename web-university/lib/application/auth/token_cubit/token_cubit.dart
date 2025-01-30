843b707509e21b94cd8abb3b66b8b2b676fd385f
JASEEMCP <jaseemcp85@gmail.com>
2025-01-30 14:57:07 +0530

    feat: update token refresh method to use POST request in both admin and university modules

--- web-university/lib/application/auth/token_cubit/token_cubit.dart
+++ web-university/lib/application/auth/token_cubit/token_cubit.dart
@@ -48,7 +48,7 @@ class TokenCubit extends Cubit<TokenState> {
 
   Future<String?> refreshToken() async {
     final response =
-        await dioClient.dio.get("${Env().apiBaseUrl}home/token/refresh/");
+        await dioClient.dio.post("${Env().apiBaseUrl}home/token/refresh/");
     if (response.statusCode == 200) {
       final token = Token.fromJson(response.data);
       pref.token.value.accessToken = token.accessToken;
